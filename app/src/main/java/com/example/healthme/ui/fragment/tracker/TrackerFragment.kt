5c5796a72fd562774e729e9481e0841dd90c1aae
osemeniha <semolga22@gmail.com>
2022-03-28 00:58:33 +0700

    add: weekly calendar

--- app/src/main/java/com/example/healthme/ui/fragment/tracker/TrackerFragment.kt
+++ app/src/main/java/com/example/healthme/ui/fragment/tracker/TrackerFragment.kt
@@ -5,10 +5,34 @@ import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
 import com.example.healthme.R
 import com.example.healthme.databinding.FragmentTrackerBinding
+import kotlinx.android.synthetic.main.fragment_tracker.*
+import java.text.SimpleDateFormat
+import java.util.*
 
-class TrackerFragment : Fragment() {
+
+class TrackerFragment : Fragment(){
+
+    private val lastDayInCalendar = Calendar.getInstance(Locale.ENGLISH)
+    private val sdf = SimpleDateFormat("MMMM yyyy", Locale.ENGLISH)
+    private val cal = Calendar.getInstance(Locale.ENGLISH)
+
+    // current date
+    private val currentDate = Calendar.getInstance(Locale.ENGLISH)
+    private val currentDay = currentDate[Calendar.DAY_OF_MONTH]
+    private val currentMonth = currentDate[Calendar.MONTH]
+    private val currentYear = currentDate[Calendar.YEAR]
+
+    // selected date
+    private var selectedDay: Int = currentDay
+    private var selectedMonth: Int = currentMonth
+    private var selectedYear: Int = currentYear
+
+    // all days in month
+    private val dates = ArrayList<Date>()
 
     private var _binding: FragmentTrackerBinding? = null
     private val binding: FragmentTrackerBinding get() = _binding!!
@@ -17,10 +41,10 @@ class TrackerFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        (activity as AppCompatActivity).supportActionBar?.title =
-            resources.getString(R.string.tracker)
+        (activity as AppCompatActivity).supportActionBar?.title = resources.getString(R.string.tracker)
         setHasOptionsMenu(true)
         _binding = FragmentTrackerBinding.inflate(inflater, container, false)
+
 
         binding.btnAdd.setOnClickListener {
             binding.btnAdd.hide()
@@ -44,7 +68,81 @@ class TrackerFragment : Fragment() {
             findNavController().navigate(R.id.to_addSymptomFragment)
         }
 
+        lastDayInCalendar.add(Calendar.MONTH, 6)
+
+        setUpCalendar()
+        binding.previousMonth!!.setOnClickListener {
+            if (cal.after(currentDate)) {
+                cal.add(Calendar.MONTH, -1)
+                if (cal == currentDate)
+                    setUpCalendar()
+                else
+                    setUpCalendar(changeMonth = cal)
+            }
+        }
+        binding.nextMonth!!.setOnClickListener {
+            if (cal.before(lastDayInCalendar)) {
+                cal.add(Calendar.MONTH, 1)
+                setUpCalendar(changeMonth = cal)
+            }
+        }
+
         return binding.root
+    }
+    private fun setUpCalendar(changeMonth: Calendar? = null) {
+        binding.monthYearText!!.text = sdf.format(cal.time)
+        val monthCalendar = cal.clone() as Calendar
+        val maxDaysInMonth = cal.getActualMaximum(Calendar.DAY_OF_MONTH)
+
+        selectedDay =
+            when {
+                changeMonth != null -> changeMonth.getActualMinimum(Calendar.DAY_OF_MONTH)
+                else -> currentDay
+            }
+        selectedMonth =
+            when {
+                changeMonth != null -> changeMonth[Calendar.MONTH]
+                else -> currentMonth
+            }
+        selectedYear =
+            when {
+                changeMonth != null -> changeMonth[Calendar.YEAR]
+                else -> currentYear
+            }
+
+        var currentPosition = 0
+        dates.clear()
+        monthCalendar.set(Calendar.DAY_OF_MONTH, 1)
+
+        while (dates.size < maxDaysInMonth) {
+            // get position of selected day
+            if (monthCalendar[Calendar.DAY_OF_MONTH] == selectedDay)
+                currentPosition = dates.size
+            dates.add(monthCalendar.time)
+            monthCalendar.add(Calendar.DAY_OF_MONTH, 1)
+        }
+
+        val layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        calendarRecyclerView!!.layoutManager = layoutManager
+        val calendarAdapter = CalendarAdapter(requireContext(), dates, currentDate, changeMonth)
+        calendarRecyclerView!!.adapter = calendarAdapter
+
+        binding.recyclerView.adapter = calendarAdapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        when {
+            currentPosition > 2 -> calendarRecyclerView!!.scrollToPosition(currentPosition - 3)
+            maxDaysInMonth - currentPosition < 2 -> calendarRecyclerView!!.scrollToPosition(currentPosition)
+            else -> calendarRecyclerView!!.scrollToPosition(currentPosition)
+        }
+
+        calendarAdapter.setOnItemClickListener(object : CalendarAdapter.OnItemClickListener {
+            override fun onItemClick(position: Int) {
+                val clickCalendar = Calendar.getInstance()
+                clickCalendar.time = dates[position]
+                selectedDay = clickCalendar[Calendar.DAY_OF_MONTH]
+            }
+        })
     }
 
     private fun hideDialogFAB() {
